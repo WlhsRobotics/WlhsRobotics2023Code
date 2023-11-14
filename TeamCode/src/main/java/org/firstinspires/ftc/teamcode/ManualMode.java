1caa18cd7e1717a561ce764b8ae2c07b00f07053
ShadySeagull <146381770+ShadySeagull@users.noreply.github.com>
2023-11-14 16:25:13 -0500

    Update ManualMode.java
    Just made things easier to read, changed a few comments like the one that said "This is stupid", I also created this crazy thing called a function to make it more organized. I am beginning to add the super complex formula for our arm. As the name suggests it is super duper complicated. That's all. Have a great day. I would add exclamation points but I am kind of sad right now. I also changed some names that were quite odd, I will likely update the variable names in the future to make them easier to read. Ok, that's everything, have a nice life. WL ROBOTICS YESSIR!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualMode.java
@@ -14,6 +14,7 @@ public class ManualMode extends LinearOpMode {
     //Declaring DcMotors and Servo vars
     DcMotor Right_Motor, Left_Motor, Turn_Table_Motor;
     Servo  webcam_rotation;
+    
     public void runOpMode() {
         //Creating vars for came controller input
         double GCrx;
@@ -25,22 +26,26 @@ public class ManualMode extends LinearOpMode {
         double GClb;
         double GCrb;
         boolean GC_A;
-        //updating the display called telemetry for user knowlege.
-        telemetry.addData("Status", "Initializing");
-        telemetry.update();
+        
         //Mapping DcMotors and Servos
         Right_Motor = hardwareMap.get(DcMotor.class, "M2");
         Left_Motor = hardwareMap.get(DcMotor.class, "M1");
-        //setting intial power = 0
+        
+        //setting intial power for drive motors to 0
         Right_Motor.setPower(0);
         Left_Motor.setPower(0);
+
+        //If we add front two drive motors
         //Motor_Three.setPower(0);
         //Motor_Four.setPower(0);
+        
         //updating telemetry
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+        
         //waiting for user to press start
         waitForStart();
+        
         while (opModeIsActive()) {
             //setting game controller vars = to game controller inputs
             GCry = -gamepad1.right_stick_y;
@@ -50,50 +55,49 @@ public class ManualMode extends LinearOpMode {
             bGClb = gamepad1.left_bumper;
             bGCrb = gamepad1.right_bumper;
             GC_A = gamepad1.a;
-            //THIS IS STUPID
+            
             //boolean to Double Conversion
-            if (!bGClb){
-                GClb = 0.0;
-            }
-            else {
-                GClb = 1.0;
-            }
-
-            if (!bGCrb){
-                GCrb = 0.0;
-            }
-            else{
-                GCrb = 1.0;
-            }
+            if (!bGClb) GClb = 0.0; else GClb = 1.0;
+            if (!bGCrb) GCrb = 0.0; else GCrb = 1.0;
+            
             //declaring new vars with limits for controller
             double Left_Power = Math.min(Math.max(GCrx+GCry, -1),1);
             double Right_Power = Math.min(Math.max(GCrx-GCry, -1),1);
+            
             //setting motor speed
             Left_Motor.setPower(Left_Power);
             Right_Motor.setPower(Right_Power);
+            
             //updating display for user knowlege and debugging purposes
-            {
-                telemetry.addData("Status", "Running");
-                telemetry.addData("Movement Stuff:", "");
-                telemetry.addData("Right Stick Y: ", GCry);
-                telemetry.addData("Right Stick X: ", GCrx);
-                telemetry.addData("Servo Stuff:", "");
-                telemetry.addData("Left Stick Y: ", GCly);
-                telemetry.addData("Left Stick X: ", GClx);
-                telemetry.addData("Servo Value", GCly-GClx);
-                telemetry.addData("Turn Table Stuff:", "");
-                telemetry.addData("Right Bumper: ", GCrb);
-                telemetry.addData("Left Bumper: ", GCrb);
-                telemetry.update();
-            }
+            debug();
+            
             resetRuntime();
         }
         //after stop is pressed, informing user of program status
         telemetry.addData("Status: ", "Stopped");
         telemetry.update();
     }
-    /**Servos have min/max positions
-     This method allows us to set servo positions and limits
-     while still allowing us to use the servo in OPmode
-     **/
+
+    
+    public void debug extends runOpMode()
+    {
+        telemetry.addData("Status", "Running");
+        telemetry.addData("Movement Stuff:", "");
+        telemetry.addData("Right Stick Y: ", GCry);
+        telemetry.addData("Right Stick X: ", GCrx);
+        telemetry.addData("Servo Stuff:", "");
+        telemetry.addData("Left Stick Y: ", GCly);
+        telemetry.addData("Left Stick X: ", GClx);
+        telemetry.addData("Servo Value", GCly-GClx);
+        telemetry.addData("Turn Table Stuff:", "");
+        telemetry.addData("Right Bumper: ", GCrb);
+        telemetry.addData("Left Bumper: ", GCrb);
+        telemetry.update();
+    }
+    
+    public double superComplexFormula extends runOpMode()
+    {
+        GCry = 1;
+    }
+
 }
